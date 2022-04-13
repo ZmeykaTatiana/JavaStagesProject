bab652a912d15248a00139eba3ec6152664a5455
ZmeykaTatiana <zacharovanaja@mail.ru>
2022-04-13 11:59:55 +0300

    Initiated the creation of project

---
+++ src/by/itstep/zmeyka/javastages/stage12/task1/controler/Main.java
@@ -0,0 +1,22 @@
+package by.itstep.zmeyka.javastages.stage12.task1.controler;
+import by.itstep.zmeyka.javastages.stage12.task1.view.Printer;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        Printer.print("Input four numbers");
+        int x1=scanner.nextInt();
+        int y1=scanner.nextInt();
+        int x2=scanner.nextInt();
+        int y2=scanner.nextInt();
+
+        boolean result=true;
+        String msg=result? "Yes":"No";
+        Printer.print(msg);
+
+
+
+    }
+}
---
+++ src/by/itstep/zmeyka/javastages/stage12/task1/model/logic/ChessLogic.java
@@ -0,0 +1,7 @@
+package by.itstep.zmeyka.javastages.stage12.task1.model.logic;
+
+public class ChessLogic {
+    public static boolean checkRookStep(int x1,int y1, int x2,int y2){
+        return false;
+    }
+}
